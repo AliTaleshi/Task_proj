ca742402266a681266250b6f2cfeb971eb0e28be
AliTaleshi <ali@DESKTOP-ASAPQPV.localdomain>
2023-03-01 18:54:31 +0330

    customer model added

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    username = models.CharField(max_length=20)
+    id = models.UUIDField(primary_key=True, editable=False)
+    credit = models.DecimalField(max_digits=15, decimal_places=2)
