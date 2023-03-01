a31aa8a1c03edb710a002e3a8eaaea1f22f40fd5
AliTaleshi <ali@DESKTOP-ASAPQPV.localdomain>
2023-03-01 19:06:46 +0330

    subscription model added

--- customers/models.py
+++ customers/models.py
@@ -5,3 +5,9 @@ class Customer(models.Model):
     username = models.CharField(max_length=20)
     id = models.UUIDField(primary_key=True, editable=False)
     credit = models.DecimalField(max_digits=15, decimal_places=2)
+
+class Subscription(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.DecimalField(max_digits=15, decimal_places=2)
+    is_active = models.BooleanField(default=False)
+    
